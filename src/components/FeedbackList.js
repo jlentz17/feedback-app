7c4d5fa0f89b506595c29e1ccfe8bc3e2d3b30b2
jlentz17 <64111084+jlentz17@users.noreply.github.com>
2022-09-02 21:04:43 -0700

    adds framer-motion animation-fade for delete/add

--- src/components/FeedbackList.js
+++ src/components/FeedbackList.js
@@ -10,11 +10,34 @@ const FeedbackList = ({ feedback, handleDelete }) => {
 
   return (
     <div className='feedback-list'>
-      {feedback.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedback.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={item.id}
+              item={item}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   )
+
+  // Without framer motion
+
+  // return (
+  //   <div className='feedback-list'>
+  //     {feedback.map((item) => (
+  //       <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+  //     ))}
+  //   </div>
+  // )
 }
 
 FeedbackList.propTypes = {
